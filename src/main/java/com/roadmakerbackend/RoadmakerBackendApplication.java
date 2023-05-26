882241fd642a3a44338350ab39fea9e5b77d9b8e
Real-Dongjun-Shin <djshin@mz.co.kr>
2023-05-26 16:32:04 +0900

    Initial commit

---
+++ src/main/java/com/roadmakerbackend/RoadmakerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roadmakerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmakerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadmakerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadmakerbackend/RoadmakerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmakerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmakerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
